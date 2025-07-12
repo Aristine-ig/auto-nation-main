55be9dfe447d02735daabd325a8b1fe9a6372de1
Aristine-ig <aristine277@gmail.com>
2025-07-12 08:54:22 +0530

    Fix Clerk Provider Error

--- src/main.jsx
+++ src/main.jsx
@@ -15,16 +15,10 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {PUBLISHABLE_KEY ? (
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <Router>
-          <App />
-        </Router>
-      </ClerkProvider>
-    ) : (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <Router>
         <App />
       </Router>
-    )}
+    </ClerkProvider>
   </React.StrictMode>
 );
